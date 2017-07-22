430bbfb21181f6933c42f037284921e65ecda94e
Gav Hanna <gavhanna@gmail.com>
2017-07-22 18:33:57 +0100

    fully working post time indicator

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -41,14 +41,29 @@ export class QuestionComponent implements OnInit {
     const oneDay = 24 * 60 * 60 * 1000;
     const firstDate = new Date(postedDate);
     const secondDate = new Date();
-    const numOfDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay))); 
+    const numOfDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+    const hours = Math.abs(secondDate.getTime() - firstDate.getTime()) / 36e5;
 
     if (numOfDays === 0) {
-      return 'Today';
+      return this.getHours(hours);
     } else if (numOfDays === 1) {
       return 'Yesterday';
     } else {
       return numOfDays + ' days ago';
+    }
+  }
+
+  getHours(hours: number) {
+    if ((hours < 0.01)) {
+      return 'Just now';
+    } else if(hours < 0.02) {
+      return '1 minute ago';
+    } else if (hours < 0.1) {
+      return hours.toString().slice(3, 4) + ' minutes ago';
+    } else if (hours < 0.6) {
+      return hours.toString().slice(2, 4) + ' minutes ago';
+    } else {
+      return ((hours / 60) * 100).toString().slice(0, 1) + ' hours ago';
     }
   }
 
